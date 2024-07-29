5c5e698dc9830d87f9a465158bbb000d366a8105
kuchaicode <kristntan@gmail.com>
2024-07-29 23:20:30 +0800

    Add ThemeProvider, PokeCard

---
+++ src/components/PokeCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
+
+type Props = {}
+
+export default function PokeCard({}: Props) {
+  return (
+    <div>PokeCard</div>
+  )
+}
+
