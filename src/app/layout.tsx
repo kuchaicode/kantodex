dfa38a8cb59f0587d881a214503dee6443d95708
kuchaicode <kristntan@gmail.com>
2024-08-01 04:20:36 +0800

    Update layout, remove unused components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import CardData from "@/components/CardData";
 import PokedataProvider from "@/components/PokedataProvider";
 import Nav from "@/components/Nav";
-import PageList from "@/components/PageList";
-
-// REMEMBER: QUERYCLIENTPROVIDER ADD LATER PLSS
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -32,21 +21,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-       <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-
-        <PokedataProvider>
-          <Nav />
-          {children} 
-        </PokedataProvider>
-        
-
-  </ThemeProvider>
-        </body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <PokedataProvider>
+            <Nav />
+            {children} 
+          </PokedataProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
